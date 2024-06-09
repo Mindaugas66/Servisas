b363cd34b55eef3f6df4dac823c3cda3ec138602
Mindaugas66 <mindaugas.bog@gmail.com>
2024-06-09 20:37:32 +0300

    added /cars/ and /orders/ urls

--- mysite/servisas/urls.py
+++ mysite/servisas/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('cars/', views.Cars, name="Cars"),
+    path('orders/', views.OrdersAndOrderRows, name="Cars"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
--- mysite/servisas/views.py
+++ mysite/servisas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import CarModel, Service, Order
+from .models import CarModel, Service, Order, Car, OrderRow
 import datetime
 
 
@@ -15,3 +15,21 @@ def index(request):
         "year": datetime.datetime.now()
     }
     return render(request, template_name="index.html", context=context)
+
+
+def Cars(request):
+    cars = Car.objects.all()
+    context = {
+        "cars": cars
+    }
+    print(cars)
+    return render(request, template_name="cars.html", context=context)
+
+def OrdersAndOrderRows(request):
+    orders = Order.objects.all().order_by('-date')
+    order_rows = OrderRow.objects.all()
+    context = {
+        "orders": orders,
+        "order_rows": order_rows
+    }
+    return render(request, template_name="orders.html", context=context)
